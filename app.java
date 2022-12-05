febba4300cefdc866b14574f5b8c78a3508d3d91
Otillia <kawumatk@gmail.com>
2022-12-05 12:32:02 +0000

    working on web pages

---
+++ app.java
@@ -0,0 +1,6 @@
+# This is code for twitters web page applications.
+# Please donot hard code.
+# Make use of variables, especially the dynamic variables.
+# Please start on some code lines tomorrow, one at atime.
+# THANK YOU; Team Leadd; Otillia.
+#

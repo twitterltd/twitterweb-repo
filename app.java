12a971594c81daa53a98f973d77a53a7ece33b00
Otillia <kawumatk@gmail.com>
2022-12-05 14:15:37 +0000

    new changes

--- app.java
+++ app.java
@@ -2,5 +2,6 @@
 # Please donot hard code.
 # Make use of variables, especially the dynamic variables.
 # Please start on some code lines tomorrow, one at atime.
+# Always remenber the best versioning practises.
 # THANK YOU; Team Leadd; Otillia.
 #
